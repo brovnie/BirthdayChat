1372bb82f6075e2c179ae607645fbbb31f4a6efe
brovnie <maarl.deenis@gmail.com>
2020-08-20 15:44:49 +0200

    made login/logout route and set up navigabtion bar

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let logger = require('morgan');
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 let registerRouter = require('./routes/api/v1/register');
+let loginRouter = require('./routes/api/v1/login');
 
 let app = express();
 
@@ -34,6 +35,7 @@ app.use(methodOverride("_method"));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/register",registerRouter);
+app.use("/login", loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/api/v1/login.js
@@ -0,0 +1,26 @@
+// ========= Login/Logout Routes
+// 
+//
+//
+
+let express = require('express');
+let router = express.Router();
+let passport = require("passport");
+let Account = require("../../../modules/account");
+
+router.get("/", (req, res) => {
+    res.render("login");
+});
+
+router.post("/", passport.authenticate("local", 
+{ successRedirect: "/login",
+    failureRedirect: "/register"
+ }), (req, res) =>{
+});
+
+router.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+module.exports = router;
